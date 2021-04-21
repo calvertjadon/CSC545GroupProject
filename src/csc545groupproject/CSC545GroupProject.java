4a95c076b31ea46720dd27758b68b0a0150f444a
Jadon Calvert <calvertjadon@gmail.com>
2021-04-21 13:31:30 -0400

    Initial commit

---
+++ src/csc545groupproject/CSC545GroupProject.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csc545groupproject;
+
+/**
+ *
+ * @author Jadon
+ */
+public class CSC545GroupProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
